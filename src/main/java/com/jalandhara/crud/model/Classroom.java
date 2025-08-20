b09d47c61dd942bf111c0a39320440de1edeac6d
Jalandhara06 <jalandharakannan@gmail.com>
2025-08-20 17:41:03 +0530

    application renamed to yaml file

--- src/main/java/com/jalandhara/crud/model/Classroom.java
+++ src/main/java/com/jalandhara/crud/model/Classroom.java
@@ -1,8 +1,12 @@
 package com.jalandhara.crud.model;
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
 public class Classroom {
     @Id
-    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
     private long id;
     private int roomNo;
     private int floorNo;
