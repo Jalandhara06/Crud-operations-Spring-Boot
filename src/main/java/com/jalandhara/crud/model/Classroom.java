39228cd92d56d71e40ec50bda6ae0e27c952f346
Jalandhara06 <133858946+Jalandhara06@users.noreply.github.com>
2025-08-20 16:05:38 +0530

    Update Classroom.java

--- src/main/java/com/jalandhara/crud/model/Classroom.java
+++ src/main/java/com/jalandhara/crud/model/Classroom.java
@@ -1,6 +1,9 @@
 package com.jalandhara.crud.model;
 
 public class Classroom {
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
     private long id;
     private int roomNo;
+    private int floorNo;
 }
